3344813af619bea1e9b79249212414e2fd05743e
Christopher Kemp <cmkemp52@gmail.com>
2019-09-19 10:16:32 -0400

    First commit

---
+++ helloworld.rb
@@ -0,0 +1,5 @@
+puts "Hello, world!"
+puts ""
+#print does not add new line while puts does add a new line.
+print "Hello,"
+puts " world!"
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is my first set of Ruby exercises to learn the language
